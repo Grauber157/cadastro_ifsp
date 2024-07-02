4150b9ad427bae89718664a26ead3ba35da01d55
Grauber157 <faria.dias@aluno.ifsp.edu.br>
2024-07-02 10:21:04 -0300

    a

--- include/conexao.php
+++ include/conexao.php
@@ -1,5 +1,17 @@
 <?php
 
-    
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "ifsp";
+$port = 3307;
+
+$link = mysqli_connect($hostname, $username, $password, $database, $port);
+
+if(mysqli_connect_errno())
+{
+    echo("Erro conexão: %s". mysqli_connect_error());
+    exit();
+}
 
 ?>
