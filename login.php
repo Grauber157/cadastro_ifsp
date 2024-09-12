4bba248af8e54ed1031ca42c930e8fad197d4576
Grauber157 <faria.dias@aluno.ifsp.edu.br>
2024-09-12 10:40:48 -0300

    a

---
+++ login.php
@@ -0,0 +1,35 @@
+<?php
+    include('include/conexao.php');
+
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = "SELECT email, senha FROM cliente WHERE email = '$email'";
+    $result = mysqli_query($con, $sql);
+
+    if(mysqli_num_rows($result) > 0)
+    {
+        //CHECAGEM SENHA
+        $row = mysqli_fetch_array($result);
+        if($row['senha'] == $senha)
+        {
+            include('inicia_sessao.php');
+            $_SESSION['login'] = $row;
+            header('location: index.php');
+        }
+        else
+        {
+            echo "<h1>Senha inválida</h1>";
+        }
+    }
+
+    else
+    {
+        echo "<h1>Usuário não encontrado</h1>";
+    }
+
+    $_SESSION['email'] = $email;
+    $_SESSION['senha'] = $senha;
+
+    
+?>
